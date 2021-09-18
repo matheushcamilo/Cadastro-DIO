4e84b25858f4cc66e75069c292a4a3c42f9f7b5b
matheushcamilo <matheushcamilo1997@gmail.com>
2021-09-18 19:14:24 -0300

    First folders and abstract class created

---
+++ Cadastros/Cadastros/Classes/EntidadeBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cadastros.Classes
+{
+    public abstract class EntidadeBase
+    {
+        public int Id { get; protected set; }
+    }
+    
+}
